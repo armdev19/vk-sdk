124a9f14a540632dbbd6057d09e75b7b641b82c1
armdev19 <armdev19@gmail.com>
2019-10-07 18:17:55 +0400

    Add dependency and Create first design - Add Moxy - Add Dagger - Add Circular Progress View

--- app/src/main/java/com/infernal93/vksdk/activities/LoginActivity.kt
+++ app/src/main/java/com/infernal93/vksdk/activities/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.infernal93.vksdk
+package com.infernal93.vksdk.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.infernal93.vksdk.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
